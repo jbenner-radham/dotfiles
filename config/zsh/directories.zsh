dce25479611cb998c4005523e2d36543fda2d3e6
James Benner <hello@jamesbenner.com>
2025-06-26 22:50:22 -0400

    Add customized libs from oh-my-zsh

---
+++ config/zsh/directories.zsh
@@ -0,0 +1,22 @@
+# Directory Options <https://zsh.sourceforge.io/Doc/Release/Options.html#Changing-Directories>
+setopt AUTO_CD            # If a command is issued that can’t be executed as a normal command, and the command is the name of a directory, perform the cd command to that directory.
+setopt AUTO_PUSHD         # Make cd push the old directory onto the directory stack.
+setopt PUSHD_IGNORE_DUPS  # Don’t push multiple copies of the same directory onto the directory stack. 
+
+# Directory Navigation Aliases
+alias -g ...='../..'
+alias -g ....='../../..'
+alias -g .....='../../../..'
+alias -g ......='../../../../..'
+alias -- -='cd -'
+
+# List Directory Aliases
+if (( $+commands[eza] )); then
+  alias la="eza --almost-all --git --icons --long"
+  alias ll="eza --git --icons --long"
+  alias ls="eza --icons"
+else
+  alias la='ls --color=auto -lAh'
+  alias ll='ls --color=auto -lh'
+  alias ls='ls --color=auto'
+fi
---
+++ config/zsh/history.zsh
@@ -0,0 +1,13 @@
+[ "$HISTSIZE" -lt 50000 ] && HISTSIZE=50000
+[ "$SAVEHIST" -lt 10000 ] && SAVEHIST=10000
+
+# History Options <https://zsh.sourceforge.io/Doc/Release/Options.html#History>
+setopt EXTENDED_HISTORY        # Save each command’s beginning timestamp (in seconds since the epoch) and the duration (in seconds) to the history file.
+setopt HIST_EXPIRE_DUPS_FIRST  # If the internal history needs to be trimmed to add the current command line, setting this option will cause the oldest history event that has a duplicate to be lost before losing a unique event from the list.
+setopt HIST_IGNORE_ALL_DUPS    # If a new command line being added to the history list duplicates an older one, the older command is removed from the list (even if it is not the previous event).
+setopt HIST_IGNORE_DUPS        # Do not enter command lines into the history list if they are duplicates of the previous event.
+setopt HIST_IGNORE_SPACE       # Remove command lines from the history list when the first character on the line is a space, or when one of the expanded aliases contains a leading space.
+setopt HIST_SAVE_NO_DUPS       # When writing out the history file, older commands that duplicate newer ones are omitted.
+setopt HIST_VERIFY             # Whenever the user enters a line with history expansion, don’t execute the line directly; instead, perform history expansion and reload the line into the editing buffer.
+setopt INC_APPEND_HISTORY      # This option works like APPEND_HISTORY except that new history lines are added to the $HISTFILE incrementally (as soon as they are entered), rather than waiting until the shell exits.
+setopt SHARE_HISTORY           # This option both imports new commands from the history file, and also causes your typed commands to be appended to the history file.
