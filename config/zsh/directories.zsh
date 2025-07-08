a5cf98af94965fe80440aabed8bac302f167f1dd
James Benner <hello@jamesbenner.com>
2025-07-08 06:57:48 -0400

    Update zsh directories script with eza alias updates

--- config/zsh/directories.zsh
+++ config/zsh/directories.zsh
@@ -1,7 +1,7 @@
 # Directory Options <https://zsh.sourceforge.io/Doc/Release/Options.html#Changing-Directories>
 setopt AUTO_CD            # If a command is issued that can’t be executed as a normal command, and the command is the name of a directory, perform the cd command to that directory.
 setopt AUTO_PUSHD         # Make cd push the old directory onto the directory stack.
-setopt PUSHD_IGNORE_DUPS  # Don’t push multiple copies of the same directory onto the directory stack. 
+setopt PUSHD_IGNORE_DUPS  # Don’t push multiple copies of the same directory onto the directory stack.
 
 # Directory Navigation Aliases
 alias -g ...='../..'
@@ -12,8 +12,8 @@ alias -- -='cd -'
 
 # List Directory Aliases
 if (( $+commands[eza] )); then
-  alias la="eza --almost-all --git --icons --long"
-  alias ll="eza --git --icons --long"
+  alias la="eza --almost-all --git --group-directories-first --icons --long"
+  alias ll="eza --git --group-directories-first --icons --long"
   alias ls="eza --icons"
 else
   alias la='ls --color=auto -lAh'
