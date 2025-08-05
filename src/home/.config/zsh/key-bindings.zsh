6bcbdee2a349b1fd56a36358cc25b758b3c8a05a
James Benner <hello@jamesbenner.com>
2025-08-05 18:32:06 -0400

    Update comments and comment out some key bindings

--- src/home/.config/zsh/key-bindings.zsh
+++ src/home/.config/zsh/key-bindings.zsh
@@ -8,9 +8,11 @@ if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
   function zle-line-init() {
     echoti smkx
   }
+
   function zle-line-finish() {
     echoti rmkx
   }
+
   zle -N zle-line-init
   zle -N zle-line-finish
 fi
@@ -19,69 +21,71 @@ fi
 bindkey -e
 
 # [PageUp] - Up a line of history
-if [[ -n "${terminfo[kpp]}" ]]; then
-  bindkey -M emacs "${terminfo[kpp]}" up-line-or-history
-  bindkey -M viins "${terminfo[kpp]}" up-line-or-history
-  bindkey -M vicmd "${terminfo[kpp]}" up-line-or-history
-fi
+# if [[ -n "${terminfo[kpp]}" ]]; then
+#   bindkey -M emacs "${terminfo[kpp]}" up-line-or-history
+#   bindkey -M viins "${terminfo[kpp]}" up-line-or-history
+#   bindkey -M vicmd "${terminfo[kpp]}" up-line-or-history
+# fi
 # [PageDown] - Down a line of history
-if [[ -n "${terminfo[knp]}" ]]; then
-  bindkey -M emacs "${terminfo[knp]}" down-line-or-history
-  bindkey -M viins "${terminfo[knp]}" down-line-or-history
-  bindkey -M vicmd "${terminfo[knp]}" down-line-or-history
-fi
+# if [[ -n "${terminfo[knp]}" ]]; then
+#   bindkey -M emacs "${terminfo[knp]}" down-line-or-history
+#   bindkey -M viins "${terminfo[knp]}" down-line-or-history
+#   bindkey -M vicmd "${terminfo[knp]}" down-line-or-history
+# fi
 
 # Start typing + [Up-Arrow] - fuzzy find history forward
-autoload -U up-line-or-beginning-search
-zle -N up-line-or-beginning-search
+# autoload -U up-line-or-beginning-search
+# zle -N up-line-or-beginning-search
 
-bindkey -M emacs "^[[A" up-line-or-beginning-search
-bindkey -M viins "^[[A" up-line-or-beginning-search
-bindkey -M vicmd "^[[A" up-line-or-beginning-search
-if [[ -n "${terminfo[kcuu1]}" ]]; then
-  bindkey -M emacs "${terminfo[kcuu1]}" up-line-or-beginning-search
-  bindkey -M viins "${terminfo[kcuu1]}" up-line-or-beginning-search
-  bindkey -M vicmd "${terminfo[kcuu1]}" up-line-or-beginning-search
-fi
+# bindkey -M emacs "^[[A" up-line-or-beginning-search
+# bindkey -M viins "^[[A" up-line-or-beginning-search
+# bindkey -M vicmd "^[[A" up-line-or-beginning-search
+# if [[ -n "${terminfo[kcuu1]}" ]]; then
+#   bindkey -M emacs "${terminfo[kcuu1]}" up-line-or-beginning-search
+#   bindkey -M viins "${terminfo[kcuu1]}" up-line-or-beginning-search
+#   bindkey -M vicmd "${terminfo[kcuu1]}" up-line-or-beginning-search
+# fi
 
 # Start typing + [Down-Arrow] - fuzzy find history backward
-autoload -U down-line-or-beginning-search
-zle -N down-line-or-beginning-search
+# autoload -U down-line-or-beginning-search
+# zle -N down-line-or-beginning-search
 
-bindkey -M emacs "^[[B" down-line-or-beginning-search
-bindkey -M viins "^[[B" down-line-or-beginning-search
-bindkey -M vicmd "^[[B" down-line-or-beginning-search
-if [[ -n "${terminfo[kcud1]}" ]]; then
-  bindkey -M emacs "${terminfo[kcud1]}" down-line-or-beginning-search
-  bindkey -M viins "${terminfo[kcud1]}" down-line-or-beginning-search
-  bindkey -M vicmd "${terminfo[kcud1]}" down-line-or-beginning-search
-fi
+# bindkey -M emacs "^[[B" down-line-or-beginning-search
+# bindkey -M viins "^[[B" down-line-or-beginning-search
+# bindkey -M vicmd "^[[B" down-line-or-beginning-search
+# if [[ -n "${terminfo[kcud1]}" ]]; then
+#   bindkey -M emacs "${terminfo[kcud1]}" down-line-or-beginning-search
+#   bindkey -M viins "${terminfo[kcud1]}" down-line-or-beginning-search
+#   bindkey -M vicmd "${terminfo[kcud1]}" down-line-or-beginning-search
+# fi
 
-# [Home] - Go to beginning of line
+# [Home] ([Fn+LeftArrow] on Mac) - Go to beginning of line.
 if [[ -n "${terminfo[khome]}" ]]; then
   bindkey -M emacs "${terminfo[khome]}" beginning-of-line
   bindkey -M viins "${terminfo[khome]}" beginning-of-line
   bindkey -M vicmd "${terminfo[khome]}" beginning-of-line
 fi
-# [End] - Go to end of line
+
+# [End] ([Fn+RightArrow] on Mac) - Go to end of line.
 if [[ -n "${terminfo[kend]}" ]]; then
-  bindkey -M emacs "${terminfo[kend]}"  end-of-line
-  bindkey -M viins "${terminfo[kend]}"  end-of-line
-  bindkey -M vicmd "${terminfo[kend]}"  end-of-line
+  bindkey -M emacs "${terminfo[kend]}" end-of-line
+  bindkey -M viins "${terminfo[kend]}" end-of-line
+  bindkey -M vicmd "${terminfo[kend]}" end-of-line
 fi
 
-# [Shift-Tab] - move through the completion menu backwards
+# [Shift+Tab] - Move through the completion menu backwards.
 if [[ -n "${terminfo[kcbt]}" ]]; then
   bindkey -M emacs "${terminfo[kcbt]}" reverse-menu-complete
   bindkey -M viins "${terminfo[kcbt]}" reverse-menu-complete
   bindkey -M vicmd "${terminfo[kcbt]}" reverse-menu-complete
 fi
 
-# [Backspace] - delete backward
+# [Backspace] ([Delete] on Mac) - Delete backward.
 bindkey -M emacs '^?' backward-delete-char
 bindkey -M viins '^?' backward-delete-char
 bindkey -M vicmd '^?' backward-delete-char
-# [Delete] - delete forward
+
+# [Delete] ([Fn+Delete] on Mac) - Delete forward.
 if [[ -n "${terminfo[kdch1]}" ]]; then
   bindkey -M emacs "${terminfo[kdch1]}" delete-char
   bindkey -M viins "${terminfo[kdch1]}" delete-char
@@ -101,18 +105,19 @@ bindkey -M emacs '^[[3;5~' kill-word
 bindkey -M viins '^[[3;5~' kill-word
 bindkey -M vicmd '^[[3;5~' kill-word
 
-# [Ctrl-RightArrow] - move forward one word
+# [Ctrl+RightArrow] - Move forward one word.
 bindkey -M emacs '^[[1;5C' forward-word
 bindkey -M viins '^[[1;5C' forward-word
 bindkey -M vicmd '^[[1;5C' forward-word
-# [Ctrl-LeftArrow] - move backward one word
+
+# [Ctrl+LeftArrow] - Move backward one word.
 bindkey -M emacs '^[[1;5D' backward-word
 bindkey -M viins '^[[1;5D' backward-word
 bindkey -M vicmd '^[[1;5D' backward-word
 
 
 bindkey '\ew' kill-region                             # [Esc-w] - Kill from the cursor to the mark
-bindkey -s '\el' 'ls\n'                               # [Esc-l] - run command: ls
+# bindkey -s '\el' 'ls\n'                               # [Esc-l] - run command: ls
 bindkey '^r' history-incremental-search-backward      # [Ctrl-r] - Search backward incrementally for a specified string. The string may begin with ^ to anchor the search to the beginning of the line.
 bindkey ' ' magic-space                               # [Space] - don't do history expansion
 
@@ -120,7 +125,7 @@ bindkey ' ' magic-space                               # [Space] - don't do histo
 # Edit the current command line in $EDITOR
 autoload -U edit-command-line
 zle -N edit-command-line
-bindkey '\C-x\C-e' edit-command-line # [Ctrl-x] > [Ctrl-e]
+bindkey '\C-x\C-e' edit-command-line # [Ctrl+x] > [Ctrl+e]
 
 # file rename magick
 bindkey "^[m" copy-prev-shell-word
