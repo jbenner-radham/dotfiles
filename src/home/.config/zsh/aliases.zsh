648a12dc0c2f707b35a2b87bb27fd56c57117b1e
James Benner <hello@jamesbenner.com>
2025-08-09 17:49:06 -0400

    Add aliases zsh config

---
+++ src/home/.config/zsh/aliases.zsh
@@ -0,0 +1,19 @@
+# Sudo
+alias sudo='sudo ' # Allow `sudo` to work with aliases.
+
+# List Directory Aliases
+if (( $+commands[eza] )); then
+  alias la="eza --almost-all --git --group-directories-first --icons --long"
+  alias ll="eza --git --group-directories-first --icons --long"
+  alias ls="eza --icons"
+else
+  alias la='ls --color=auto -lAh'
+  alias ll='ls --color=auto -lh'
+  alias ls='ls --color=auto'
+fi
+
+# Pretty Print Environment Variables
+alias penv='print -ol $(env)'
+
+# Pretty Print Path
+alias ppath='print -l ${path}'
--- src/home/.config/zsh/directories.zsh
+++ src/home/.config/zsh/directories.zsh
@@ -1,7 +1,7 @@
 # Directory Options <https://zsh.sourceforge.io/Doc/Release/Options.html#Changing-Directories>
 setopt AUTO_CD            # Type directory name to cd into it..
 setopt AUTO_PUSHD         # Make cd push the old directory onto the directory stack.
-setopt PUSHD_IGNORE_DUPS  # Don’t push multiple copies of the same directory onto the directory stack. 
+setopt PUSHD_IGNORE_DUPS  # Don’t push multiple copies of the same directory onto the directory stack.
 
 # Directory Navigation Aliases
 alias -g ...='../..'
@@ -9,14 +9,3 @@ alias -g ....='../../..'
 alias -g .....='../../../..'
 alias -g ......='../../../../..'
 alias -- -='cd -'
-
-# List Directory Aliases
-if (( $+commands[eza] )); then
-  alias la="eza --almost-all --git --group-directories-first --icons --long"
-  alias ll="eza --git --group-directories-first --icons --long"
-  alias ls="eza --icons"
-else
-  alias la='ls --color=auto -lAh'
-  alias ll='ls --color=auto -lh'
-  alias ls='ls --color=auto'
-fi
