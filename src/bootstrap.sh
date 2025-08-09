f4fb9602bf9934e95cc11c5dca2f22435aed3407
James Benner <hello@jamesbenner.com>
2025-08-08 21:12:52 -0400

    Add build-essential install to bootstrap

--- src/bootstrap.sh
+++ src/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 if [ "$(uname)" = 'Linux' ] && [ "$(basename "${SHELL}")" != 'zsh' ]; then
-  sudo apt install --assume-yes curl git zsh
+  sudo apt install --assume-yes build-essential curl git zsh
   chsh -s "$(which zsh)"
   printf 'Restart and re-run this script to continue.\n'
   exit
