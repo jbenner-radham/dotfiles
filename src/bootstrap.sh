3af1d0840b1897a75c22127bc1363c49dfbb4043
James Benner <hello@jamesbenner.com>
2025-08-11 14:14:44 -0400

    Add has command function to bootstrap script

--- src/bootstrap.sh
+++ src/bootstrap.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
+has_command() {
+  command -v "${1}" >/dev/null 2>&1
+}
+
 if [ "$(uname)" = 'Linux' ] && [ "$(basename "${SHELL}")" != 'zsh' ]; then
   sudo apt install --assume-yes build-essential curl git zsh
   chsh -s "$(which zsh)"
