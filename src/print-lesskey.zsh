079ddc226a4c8413d63c21a76647475f65d401f2
James Benner <hello@jamesbenner.com>
2025-08-19 21:46:11 -0400

    Refine print-lesskey script

--- src/print-lesskey.zsh
+++ src/print-lesskey.zsh
@@ -1,7 +1,119 @@
 #!/usr/bin/env zsh
 
+# See: https://gist.github.com/JBlond/2fea43a3049b38287e5e9cefc87b2124
+
+# Bold
+# tput bold | cat -v
+# >>> ^[[1m
+#
+# Green
+# tput setaf 2 | cat -v
+# >>> ^[[32m
+#
+# Bold Green
+# (No tput command?)
+# >>> ^[[1;32m
+#
+# Underline
+# tput smul | cat -v
+# >>> ^[[4m
+
+function print-error() {
+  printf '%s%s%s\n' $(tput sgr1) $1 $(tput sgr0) >&2
+}
+
+function bold-color-code() {
+  zmodload zsh/pcre
+
+  setopt REMATCH_PCRE
+
+  if (( # != 1 )); then
+    print-error 'Exactly one color code argument is required.'
+    exit 1
+  fi
+
+  local -r bold="$(tput bold | cat -v)"
+  local -r color="$(tput setaf $1 | cat -v)"
+  local -r pattern='(\d+)'
+  local bold_digit
+  local color_digit
+
+  pcre_compile -smx $pattern
+  pcre_match -b -- $bold
+
+  if (( ? == 0 )); then
+    bold_digit=$MATCH
+  else
+    print "no match"
+  fi
+
+  pcre_match -b -- $color
+
+  if (( ? == 0 )); then
+    color_digit=$MATCH
+  else
+    print "no match"
+  fi
+
+  print "^[[${bold_digit};${color_digit}m"
+}
+
+function bold-underline-color-code() {
+  zmodload zsh/pcre
+
+  setopt REMATCH_PCRE
+
+  if (( # != 1 )); then
+    print-error 'Exactly one color code argument is required.'
+    exit 1
+  fi
+
+  local -r bold="$(tput bold | cat -v)"
+  local -r color="$(tput setaf $1 | cat -v)"
+  local -r underline="$(tput smul | cat -v)"
+  local -r pattern='(\d+)'
+  local bold_digit
+  local underline_digit
+  local color_digit
+
+  pcre_compile -smx $pattern
+  pcre_match -b -- $bold
+
+  if (( ? == 0 )); then
+    bold_digit=$MATCH
+  else
+    print "no match"
+  fi
+
+  pcre_match -b -- $underline
+
+  if (( ? == 0 )); then
+    underline_digit=$MATCH
+  else
+    print "no match"
+  fi
+
+  pcre_match -b -- $color
+
+  if (( ? == 0 )); then
+    color_digit=$MATCH
+  else
+    print "no match"
+  fi
+
+  print "^[[${bold_digit};${underline_digit};${color_digit}m"
+}
+
+local -r bold_green=$(bold-color-code 2)
+local -r bold_yellow=$(bold-color-code 3)
+local -r bold_underline_red=$(bold-underline-color-code 1)
+
 cat -v << EOF
 #env
+
+# Output raw color control characters for styling.
+# TODO: This seems unneeded on macOS, need to verify...
+# LESS = -R
 
 # Configure text styling when using \`less\` as a man pager.
 # See: \`man 5 terminfo\` (search "TCap Code" e.g., mb, md, etc.)
@@ -21,10 +133,10 @@ cat -v << EOF
 # | White   | 7     |
 
 # Turn on blinking.
-LESS_TERMCAP_mb = $(tput blink)
+# LESS_TERMCAP_mb = $(tput blink)
 
 # Turn on bold (extra bright) mode.
-LESS_TERMCAP_md = $(tput bold; tput setaf 2) # Bold green.
+LESS_TERMCAP_md = $bold_green
 
 # Turn off all attributes.
 LESS_TERMCAP_me = $(tput sgr0)
@@ -36,14 +148,14 @@ LESS_TERMCAP_mh = $(tput dim)
 LESS_TERMCAP_mr = $(tput rev) # Invert colors.
 
 # Exit standout mode.
-LESS_TERMCAP_se = $(tput rmso; tput sgr0)
+# LESS_TERMCAP_se = $(tput rmso; tput sgr0)
 
 # Begin standout mode.
-LESS_TERMCAP_so = $(tput bold; tput setaf 3) # Bold yellow.
+LESS_TERMCAP_so = $bold_yellow
 
 # Exit underline mode.
-LESS_TERMCAP_ue = $(tput rmul; tput sgr0)
+# LESS_TERMCAP_ue = $(tput rmul; tput sgr0)
 
 # Begin underline mode.
-LESS_TERMCAP_us = $(tput smul; tput bold; tput setaf 1) # Underlined bold red.
+LESS_TERMCAP_us = $bold_underline_red
 EOF
