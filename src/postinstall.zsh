42d90348685ff5e0e7c365d8a8e8b613daa067d7
James Benner <hello@jamesbenner.com>
2026-01-30 20:31:29 -0500

    Refine postinstall script

--- src/postinstall.zsh
+++ src/postinstall.zsh
@@ -10,11 +10,18 @@ function {
   # Enable this to assist with debugging errors.
   # setopt XTRACE  # Print commands and their arguments as they are executed.
 
+  function is-claude-plugin-enabled() {
+    local -r plugin="${1}"
+
+    claude plugin list --json \
+      | jq --arg plugin "${plugin}" '.[] | select(.id == ''$plugin'').enabled'
+  }
+
   if (( $+commands[claude] )); then
     if (( $+commands[rust-analyzer] )); then
       claude plugin install rust-analyzer-lsp@claude-plugins-official
 
-      if [[ $(claude plugin list --json | jq '.[] | select(.id == "typescript-lsp@claude-plugins-official").enabled') != true ]]; then
+      if [[ $(is-claude-plugin-enabled 'rust-analyzer-lsp@claude-plugins-official') != true ]]; then
         claude plugin enable rust-analyzer-lsp@claude-plugins-official
       fi
     fi
@@ -22,7 +29,7 @@ function {
     if (( $+commands[typescript-language-server] )); then
       claude plugin install typescript-lsp@claude-plugins-official
 
-      if [[ $(claude plugin list --json | jq '.[] | select(.id == "typescript-lsp@claude-plugins-official").enabled') != true ]]; then
+      if [[ $(is-claude-plugin-enabled 'typescript-lsp@claude-plugins-official') != true ]]; then
         claude plugin enable typescript-lsp@claude-plugins-official
       fi
     fi
